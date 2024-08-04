d3d6e6b807eceb7d477877b2b4b81dcc14dd0002
Han Myat Thu <hanmyatthu111@gmail.com>
2024-08-04 20:09:52 +0700

    filter status bar

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,48 +1,71 @@
-import React from 'react'
+import React from "react";
 
-import prisma from '@/prisma/db'
-import DataTable from './datatable';
-import Link from 'next/link';
-import { buttonVariants } from '@/components/ui/button';
-import { Pagination } from '@/components/pagination';
+import prisma from "@/prisma/db";
+import DataTable from "./datatable";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { Pagination } from "@/components/pagination";
+import { StatusFilter } from "@/components/status-filter";
+import { Status } from "@prisma/client";
 
 interface searchParams {
-  searchParams: { page: string }
+  searchParams: { page: string; status: Status };
 }
 
-const Tickets = async ({
-  searchParams,
-}: searchParams) => {
+const Tickets = async ({ searchParams }: searchParams) => {
 
-  const ticketscount = await prisma.ticket.count();
-
-  const pageSize = 10
+  const pageSize = 10;
 
   const page = parseInt(searchParams.page) || 1;
 
+  const statuses = Object.values(Status);
+
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {}
+
+  if (status) {
+    where = {
+      status,
+    }
+  } else {
+    where = {
+      NOT: [{ status: "CLOSED" as Status }]
+    }
+  }
+
   const tickets = await prisma.ticket.findMany({
+    where,
     take: pageSize,
     skip: (page - 1) * pageSize,
   });
-  
+
+  const ticketscount = await prisma.ticket.count({
+    where,
+  });
+
+
   return (
     <div>
-      <Link
-        href="/tickets/new"
-        className={buttonVariants({ variant: "default" })}
-      >
-        New Ticket
-      </Link>
-      <DataTable 
-        tickets={tickets}
-      />
-      <Pagination 
+      <div className="flex justify-between">
+        <Link
+          href="/tickets/new"
+          className={buttonVariants({ variant: "default" })}
+        >
+          New Ticket
+        </Link>
+        <StatusFilter />
+      </div>
+      <DataTable tickets={tickets} />
+      <Pagination
         itemCount={ticketscount}
         pageSize={pageSize}
         currentPage={page}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Tickets
+export default Tickets;
---
+++ components/status-filter.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+interface statusInterface {
+  label: string;
+  value?: string;
+}
+
+const statuses: statusInterface[] = [
+  { label: "Open / Started" }, 
+  { label: "Open", value: "OPEN" },
+  { label: "Started", value: "STARTED" },
+  { label: "Closed", value: "CLOSED" },
+]
+
+export const StatusFilter = () => {
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+
+  return (
+    <Select
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+
+        if (status) params.append("status", status)
+
+        const query = params.size ? `?${params.toString()}` : "0"
+        router.push(`/tickets${query}`)
+      }}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Filter By Status..." />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {statuses.map(status => (
+            <SelectItem key={status.value || "0"} value={status.value || "0"}>
+              {status.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+
+  )
+}
